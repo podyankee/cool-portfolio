57fc72698eae5c11e4b4f8ed2981c89da35b1110
podyankee <you@example.com>
2015-05-19 05:14:49 +0300

    Gallery added jquery

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,6 +9,33 @@ $(".btn_mnu").click(function() {
   $(".left_side").toggleClass("active");
 });
 	
+	var wall = new freewall(".gallery");
+wall.reset({
+	selector: "a",
+	animate: true,
+	cellW: 150,
+	cellH: "auto",
+	gutterX : 5,
+	gutterY : 5,
+	onResize: function() {
+		wall.fitWidth();
+	}
+});
+
+var images = wall.container.find("a");
+images.find("img").load(function() {
+	wall.fitWidth();
+});
+
+// $(".filter-label").click(function() {
+// 	$(".filter-label").removeClass("active");
+// 	var filter = $(this).addClass("active").data("filter");
+// 	wall.filter(filter);
+// 	setTimeout(function() {
+// 		$(window).resize();
+// 		wall.fitWidth();
+// 	}, 400);
+// });
 	
 	//Цели для Яндекс.Метрики и Google Analytics
 	$(".count_element").on("click", (function() {
